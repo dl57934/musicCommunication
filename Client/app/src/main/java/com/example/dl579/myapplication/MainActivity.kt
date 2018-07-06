af75eab4e4e7e01d929ecab42bc43e576e986b1f
LeeSangHoon <dl57934@naver.com>
2018-07-07 01:39:10 +0900

    complete choose Music

--- Client/app/src/main/java/com/example/dl579/myapplication/MainActivity.kt
+++ Client/app/src/main/java/com/example/dl579/myapplication/MainActivity.kt
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
-
+import android.net.Uri
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
     val MUSIC_REQUEST = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +27,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == MUSIC_REQUEST && resultCode== Activity.RESULT_OK && data)
+        if (requestCode == MUSIC_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            var path: Uri = data.data
+            Log.e("check", path.toString())
+        }
     }
 }
