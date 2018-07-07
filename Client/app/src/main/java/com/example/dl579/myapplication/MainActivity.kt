09e19fbda1ca4af7ab5a25f395ae3ddd27e8eb72
LeeSangHoon <dl57934@naver.com>
2018-07-07 17:11:55 +0900

    check Permission

--- Client/app/src/main/java/com/example/dl579/myapplication/MainActivity.kt
+++ Client/app/src/main/java/com/example/dl579/myapplication/MainActivity.kt
@@ -8,11 +8,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
-import java.io.File
+import android.Manifest
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        permissonCheck()
     }
    fun sendMusic(v: View){
         VolleyService.volleyFuctions(this, file.path)
@@ -32,7 +35,17 @@ class MainActivity : AppCompatActivity() {
         var intent =  Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI)
         startActivityForResult(intent, MUSIC_REQUEST)
     }
+   fun permissonCheck(){
+       if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+           if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
 
+           } else {
+               ActivityCompat.requestPermissions(this,
+                       arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE),
+                       1)
+           }
+       }
+   }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == MUSIC_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
