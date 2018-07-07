025b0284d3a97965150da1b265ca3122a8523fb7
LeeSangHoon <dl57934@naver.com>
2018-07-07 19:18:03 +0900

    complete change 16

--- Client/app/src/main/java/com/example/dl579/myapplication/controlVolley.kt
+++ Client/app/src/main/java/com/example/dl579/myapplication/controlVolley.kt
@@ -1,6 +1,7 @@
 package com.example.dl579.myapplication
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.util.Log
 import android.view.ContextMenu
 import com.android.volley.Request
@@ -9,6 +10,7 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.JsonObject
 import org.json.JSONObject
+import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
@@ -16,28 +18,28 @@ import java.io.FileInputStream
 
 object VolleyService {
     var url = "http://192.168.43.47:3000/test"
+    var maxBuffer = 1 * 1024 * 1024
     fun volleyFuctions(context:Context, path:String?) {
         var queue = Volley.newRequestQueue(context)
         var sendData = JSONObject()
-        val musicData = musicFileChange(path)
         sendData.put("groupName", "maroon5 Fans")
-        sendData.put("musicFile", musicData.toString())
-        var jsonRequest = JsonObjectRequest(Request.Method.GET, url, sendData, Response.Listener {response ->
+        sendData.put("musicFile", file2Byte(path))
+        var req = JsonObjectRequest(Request.Method.POST, url, sendData, Response.Listener {response ->
             Log.e("success", response["soccerTeam"].toString())
         }, Response.ErrorListener {
-
         })
-
-        queue.add(jsonRequest)
+        queue.add(req)
+    }
+    fun file2Byte(path:String?): ByteArray {
+        var file = File(path)
+        var size = file.length().toInt()
+        var bytes = ByteArray(size)
+        var buf = BufferedInputStream(FileInputStream(file))
+        buf.read(bytes, 0, bytes.size)
+        Log.e("ㅁㄴㅇㅁㄴㅇ: ",java.math.BigInteger(bytes).toString(16))
+        return bytes
     }
 
-    fun musicFileChange(path:String?): ByteArray {
-        val chunkSize = 1024*96
-        val file = File(path)
-        var byteData = file.readBytes()
-        return byteData
-
-    }
 }
 
 
--- Server/route/getVolleyData.js
+++ Server/route/getVolleyData.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+router.post('/',(req, res)=>{
+   let groupName = req.body;
+   let savePath = "./music/test.mp3";
+   console.log(toHexString(groupName['musicFile']));
+   fs.writeFile(savePath, toHexString(groupName['musicFile']) ,null, (err, data)=>{
 
-router.get('/',(req, res)=>{
-   var groupName = req.params.groupName;
-   console.log(groupName);
+   });
+   res.send({'soccerTeam':'Arsenal'})
 });
 
+toHexString = (byteArray)=>{
+    return Array.from(byteArray, function(byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+};
 module.exports = router;
